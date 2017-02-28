7bd53c79bc4b375043130bae334f0c6ae1359750
Guru <swati.guru@capgemini.com>
2017-02-28 10:58:26 +0530

    commit

---
+++ src/demo/NewTest.java
@@ -0,0 +1,30 @@
+package demo;		
+	
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;	
+	    
+		@BeforeTest
+		public void beforeTest() {	
+			System.setProperty("webdriver.chrome.driver","./lib/chromedriver.exe");
+		    driver = new ChromeDriver();  
+		    
+		}
+		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://www.google.com");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Google")); 		
+		}	
+			
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}
